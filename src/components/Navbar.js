0bdb9f5ad6bfcb8c783c0bbf80be88dcb16815f0
Keshav Goyal <keshavdikhani@gmail.com>
2023-06-18 23:47:11 +0530

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(prop) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${prop.mode} bg-${prop.mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">{prop.title}</a>
+        <Link className="navbar-brand" to="/">{prop.title}</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
